b7700e9cc6768183c13168d0b31177e9dff82fc5
Francis <adeboye.francis@icloud.com>
2026-06-20 11:19:11 +0100

    infra: updated terraform provisioned infra with vpc setup

--- terraform/aws-infra/outputs.tf
+++ terraform/aws-infra/outputs.tf
@@ -8,3 +8,19 @@ output "s3_bucket_arn" {
   value = aws_s3_bucket.s3_bucket_name.arn
 
 }
+
+
+output "public_subnets_id" {
+  value = [for s in aws_subnet.public_subnets : s.id]
+
+}
+
+output "private_subnets_id" {
+  value = [for s in aws_subnet.private_subnets : s.id]
+
+}
+
+output "public_subnets_map" {
+  value = { for az, subnet in aws_subnet.public_subnets : az => subnet.id }
+
+}
